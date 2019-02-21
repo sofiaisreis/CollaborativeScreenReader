61a95f6ec2102a7dc092c30a00c0ef1c57e2c086
sofiaisreis <sofia.reis@ist.utl.pt>
2019-02-21 02:12:15 +0000

    Click on Squares Class (w/sound)

---
+++ TrianglesAndSquares/Assets/Scripts/DisappearSquares.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ClassDemo.CountObjects;
+
+namespace ClassDemo {
+    public class DisappearSquares : MonoBehaviour
+    {
+        public AudioClip quadrado;
+        public AudioClip selected;
+        public AudioSource myAudioSource;
+        
+        void Start()
+        {
+            myAudioSource = GetComponent<AudioSource>();
+            //GameObject[] max_squares = GameObject.FindGameObjectsWithTag("square");
+        }
+
+        private void Update()
+        {
+        }
+
+        public void OnMouseEnter()
+        {
+            //print("Mouse is over one Square."); //On table exists " + max_squares.Length);
+            gameObject.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+            // Read name of object
+            myAudioSource.PlayOneShot(quadrado);
+        }
+
+        public void OnMouseExit()
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+
+        }
+
+        public void OnMouseDown()
+        {
+            //Read how many
+            myAudioSource.PlayOneShot(selected);
+
+            //Increment
+            print("Selected " + CountObjects.IncrementSquares() + " square a total of " + CountObjects.getsquaInit());
+
+            //hide
+            gameObject.SetActive(false);
+            //Destroy(this.gameObject);
+        }
+
+    }
+}
