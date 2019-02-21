8aa158cb23ede446124c27a1cb37463850fea2f8
sofiaisreis <sofia.reis@ist.utl.pt>
2019-02-21 02:11:21 +0000

    Counter Class

---
+++ TrianglesAndSquares/Assets/Scripts/CountObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClassDemo{
+        public static class CountObjects {
+            static int squares = 0;
+            static int triangles = 0;
+            static int triInit = GameObject.FindGameObjectsWithTag("triangle").Length;
+            static int squaInit = GameObject.FindGameObjectsWithTag("square").Length;
+
+            public static int IncrementSquares() { squares++; return squares; }
+
+            public static int IncrementTriangles() { triangles++; return triangles; }
+
+            public static int gettriInit() { return triInit; }
+
+            public static int getsquaInit() { return squaInit; }
+        
+    }
+}
