c3e6f468768197312c52f3dccc3187586bc0c7ef
sofiaisreis <sofia.reis@ist.utl.pt>
2019-05-22 22:54:18 +0100

    Just 2 touches, 2 hands - it's possible to steal touch

--- SquaresAndCircles/Assets/Scripts/NewTouch.cs
+++ SquaresAndCircles/Assets/Scripts/NewTouch.cs
@@ -32,20 +32,20 @@ public class NewTouch : MonoBehaviour
                     MyTouch ourT1 = Hand1.GetComponent<UserHand>().userTouch.touch;
                     MyTouch ourT2 = Hand2.GetComponent<UserHand>().userTouch.touch;
                     // more 2 touches, since we have 4 hands
-                     MyTouch ourT3 = Hand1.GetComponent<UserHand>().userTouch.touch;
-                     MyTouch ourT4 = Hand2.GetComponent<UserHand>().userTouch.touch;
-
+                    /* MyTouch ourT3 = Hand3.GetComponent<UserHand>().userTouch.touch;
+                     MyTouch ourT4 = Hand4.GetComponent<UserHand>().userTouch.touch;
+                     */
 
                     //Nada esta a tocar ainda
-                    if (ourT1 == null && ourT2 == null && ourT3 == null && ourT4 == null)
+                    if (ourT1 == null && ourT2 == null)// && ourT3 == null && ourT4 == null)
                     {
                         //Se estah mais perto da hand1, associar a hand1. CC o oposto
                         GameObject touchGO = Instantiate(TouchInput, Vector3.zero, Quaternion.identity);
                         touchGO.GetComponent<MyTouch>().Init(touch);
                         Vector3 hand1Pos = Hand1.transform.position;
                         Vector3 hand2Pos = Hand2.transform.position;
-                        Vector3 hand3Pos = Hand3.transform.position;
-                        Vector3 hand4Pos = Hand4.transform.position;
+                        //Vector3 hand3Pos = Hand3.transform.position;
+                        //Vector3 hand4Pos = Hand4.transform.position;
                         Vector3 touchPos = touchGO.transform.position;
                         //print("Touch Position:" + touchPos);
 
@@ -53,13 +53,13 @@ public class NewTouch : MonoBehaviour
                         if (Vector3.Distance(hand1Pos, touchPos) <= Vector3.Distance(hand2Pos, touchPos))
                         {
                             Hand1.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
-                            Hand3.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
+                            //Hand3.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
                             //print("entrei 1: A");
                         }
                         else
                         {
                             Hand2.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
-                            Hand4.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
+                            //Hand4.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
                             //print("entrei 2: A");
                         }
                     }
@@ -68,7 +68,7 @@ public class NewTouch : MonoBehaviour
                         GameObject touchGO = Instantiate(TouchInput, Vector3.zero, Quaternion.identity);
                         touchGO.GetComponent<MyTouch>().Init(touch);
                         Hand2.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
-                        Hand4.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
+                        //Hand4.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
                         //print("entrei 2: B");
                     }
                     else if (ourT1 == null && ourT2 != null)
@@ -76,7 +76,7 @@ public class NewTouch : MonoBehaviour
                         GameObject touchGO = Instantiate(TouchInput, Vector3.zero, Quaternion.identity);
                         touchGO.GetComponent<MyTouch>().Init(touch);
                         Hand1.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
-                        Hand3.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
+                       // Hand3.GetComponent<UserHand>().userTouch.NewTouchStarts(touchGO);
                         //print("entrei 1: C");
                     }
                 }
